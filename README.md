913a11fcfb1a17ed3b32dd497cde33ac21421910
Mathias Haugsbø <mathiash98@gmail.com>
2021-11-18 22:58:23 +0100

    Visualize building import and NVDB mangler

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # progress-visualizer
+
 Visualizing progress of various projects in the norwegian mapping community
+
+### Made possible with:
+
+- Norwegian mapping community https://wiki.openstreetmap.org/wiki/Norway
+- OpenStreetMap https://www.openstreetmap.org/
+- Leaflet https://leafletjs.com/reference.html
+- Mapbox https://www.mapbox.com/
+- Cors anywhere helper: Allorigins https://allorigins.win/
---
+++ main.js
@@ -0,0 +1,244 @@
+const map = L.map("map", {
+  zoomControl: false,
+}).setView([64.69135656676626, 10.795899331569672], 6);
+
+L.control
+  .zoom({
+    position: "topright",
+  })
+  .addTo(map);
+
+L.tileLayer(
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: "mapbox/dark-v10",
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken:
+      "pk.eyJ1IjoibWF0aGlhc2g5OCIsImEiOiJja3c1ZGx6bmcwZmQyMm5sajJrZGQwdDF5In0.Vw5JcsEGSmSzYTVGzhHPNQ",
+  }
+).addTo(map);
+
+init();
+
+async function init() {
+  const kommuner = await getKommuner();
+  /** @type {L.geoJSON} */
+  const kommuneLayer = renderKommuner(kommuner);
+
+  const progressSelectorRef = document.getElementById("progress-selector");
+  progressSelectorRef.addEventListener("change", (e) =>
+    handleProgressSelectorChange(e.target.value, kommuneLayer)
+  );
+
+  console.debug(progressSelectorRef);
+  handleProgressSelectorChange(progressSelectorRef.value, kommuneLayer);
+}
+
+/**
+ *
+ * @param {{"building" | "nvdb" | "stedsnavn"}} progressToVisualize
+ */
+async function handleProgressSelectorChange(progressToVisualize, kommuneLayer) {
+  switch (progressToVisualize) {
+    case "building":
+      const buildingImportProgress = await getBuildingImportProgress();
+      renderKommuneProgress(
+        kommuneLayer,
+        getKommuneProgress(buildingImportProgress, "Polygon_progress")
+      );
+      break;
+    case "nvdb":
+      const progress = await getNVDBManglerProgress();
+      renderKommuneProgress(
+        kommuneLayer,
+        getKommuneProgress(progress, "Percent_missing", true)
+      );
+      break;
+    case "stedsnavn":
+      break;
+    default:
+      console.debug(`${progressToVisualize} is not supported`);
+      break;
+  }
+}
+
+/**
+ *
+ * @param {{Id: string,}}} progress
+ */
+function renderProgress(progress) {
+  const progressEl = document.querySelector("#progress");
+  progressEl.textContent = progress;
+}
+
+/**
+ * @param {KommunerGeoJson} kommuner
+ * @return {L.geoJSON}
+ */
+function renderKommuner(kommuner) {
+  return L.geoJSON(kommuner, {
+    style: {
+      color: "#ff0000",
+      weight: 1,
+      fillColor: "#fff",
+      fillOpacity: 0.1,
+    },
+  }).addTo(map);
+}
+
+/**
+ *
+ * @param {Object} progress
+ * @param {string} progressColumn
+ * @param {bool} reverseScale
+ * @return {{[id: string]: number}}
+ */
+function getKommuneProgress(progress, progressColumn, reverseScale = false) {
+  /**@type {{[id: string]: number}} */
+  const kommuneIdToProgress = {};
+
+  for (const kommune of progress) {
+    const progressAsNumber = Number(kommune[progressColumn].match(/\d+/)[0]);
+    kommuneIdToProgress[kommune.Id] = reverseScale
+      ? 100 - progressAsNumber
+      : progressAsNumber;
+  }
+
+  return kommuneIdToProgress;
+}
+
+/**
+ * @param {L.geoJson} kommuneLayer
+ * @param {{[id: string]: KommuneProgress}} kommuneProgress
+ */
+function renderKommuneProgress(kommuneLayer, kommuneProgress) {
+  kommuneLayer.eachLayer((layer) => {
+    const kommuneId = layer.feature.properties.kommunenummer;
+    const progress = kommuneProgress[kommuneId];
+    layer.setStyle({
+      fillColor: getColor(progress),
+      color: getColor(progress),
+    });
+  });
+}
+
+/**
+ *
+ * @param {number} value from 0 to 1
+ * @returns {string} Color from red to green as hsl
+ */
+function getColor(value) {
+  //value from 0 to 1
+  var hue = ((1 - value) * 120).toString(10);
+  return ["hsl(", hue, ",100%,50%)"].join("");
+}
+
+/**
+ * @return {Promise<KommunerGeoJson>}
+ */
+async function getKommuner() {
+  const resp = await fetch("./data/kommuner.geojson");
+
+  if (resp.ok) {
+    return resp.json();
+  }
+}
+
+/**
+ * @return {Promise<KommuneBuildingProgress[]>}
+ */
+function getBuildingImportProgress() {
+  return convertWikiToJson(
+    "https://wiki.openstreetmap.org/wiki/Import/Catalogue/Norway_Building_Import/Progress"
+  );
+}
+
+/**
+ * @return {Promise<Object[]>}
+ */
+function getNVDBManglerProgress() {
+  return convertWikiToJson(
+    "https://wiki.openstreetmap.org/wiki/Import/Catalogue/Road_import_(Norway)/Update"
+  );
+}
+
+/**
+ *
+ * @param {string} url
+ * @returns
+ */
+async function convertWikiToJson(url) {
+  const resp = await fetch(
+    `https://api.allorigins.win/get?url=${encodeURIComponent(url)}`
+  );
+
+  if (resp.ok) {
+    // parse html table and get percentage for each kommune
+    const progressDocumentText = await resp.text();
+    const progressHtml = new DOMParser().parseFromString(
+      progressDocumentText,
+      "text/html"
+    );
+    const progressTable = progressHtml.querySelector("table");
+    const tableAsJson = parseHTMLTableElem(progressTable);
+    console.dir(tableAsJson);
+    return tableAsJson;
+  }
+}
+
+/**
+ *
+ * @param {HTMLTableElement} tableEl
+ * @returns {Object}
+ */
+function parseHTMLTableElem(tableEl) {
+  const columns = Array.from(tableEl.querySelectorAll("th")).map((it) =>
+    it.textContent.replace(/\\n/g, "").replace(/\s/g, "_")
+  );
+  const rows = tableEl.querySelectorAll("tbody > tr");
+  const tableAsJson = [];
+  rows.forEach((row) => {
+    const rowObject = {};
+    const cells = Array.from(row.querySelectorAll("td"));
+    for (let i = 0; i < columns.length; i++) {
+      const cell = cells[i];
+      if (cell && cell.textContent) {
+        rowObject[columns[i]] = cell.textContent.replace(/\\n/g, "");
+      }
+    }
+    if (Object.keys(rowObject).length > 0) {
+      tableAsJson.push(rowObject);
+    }
+  });
+  return tableAsJson;
+}
+
+/**
+ * @typedef {object} KommunerGeoJson
+ * @property {object[]} features
+ * @property {object} features.geometry
+ * @property {array[]|number[]} features.geometry.coordinates
+ * @property {string} features.geometry.type
+ * @property {object} features.properties
+ * @property {string} features.properties.kommunenavn
+ * @property {string} features.properties.kommunenummer
+ * @property {string} features.type
+ * @property {string} type
+ */
+
+/**
+ * @typedef {object} KommuneBuildingProgress
+ * @property {string} Id
+ * @property {string} Municipality
+ * @property {string} County
+ * @property {string} Matrikkel_buildings
+ * @property {string} OSM_buildings
+ * @property {string} Total_progress
+ * @property {string} Polygon_progress
+ * @property {string} Responsible_user(s)
+ * @property {string} Status
+ */
